73c5b7ab9ff2855b913a154c99e88e5fc4dc57bf
SoSaymon <szymon.chirowski@protonmail.com>
2022-12-25 19:48:16 +0100

    Pre-release version of mobile header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,17 +1,25 @@
+import {useState} from "react";
+
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {faBars, faXmark} from "@fortawesome/free-solid-svg-icons";
 
 import '../styles/header.scss';
 
 import logo from '../img/logo.svg';
+
 export const Header = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	}
 	return (
-		<header className={'bg-black header'}>
-			<div className={'flex p-5 justify-between items-center container mx-auto header__container'}>
+		<header className={'bg-smoky-black header'}>
+			<div className={'flex justify-between items-center p-5 mx-auto container header__container'}>
 				<span className={'w-1/3 container__spacer'}></span>
 				<span className={'flex justify-center items-center w-1/3 container__logo-container'}><img src={logo} alt={'logo'} className={'logo-container__logo'}/></span>
 				<span className={'flex items-center justify-end w-1/3 container__hamburger-container'}>
-					<FontAwesomeIcon icon={faBars} className={'text-white hamburger-container__hamburger'}/>
+					<FontAwesomeIcon icon={isMenuOpen ? faXmark : faBars} className={'text-lotion hamburger-container__hamburger'} onClick={toggleMenu}/>
 				</span>
 			</div>
 		</header>
