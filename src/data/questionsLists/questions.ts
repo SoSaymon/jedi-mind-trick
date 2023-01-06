36eb6b44a23f6c7b3f4df51647235897a27be753
SoSaymon <szymon.chirowski@protonmail.com>
2023-01-06 02:21:03 +0100

    Created template for questions

---
+++ src/data/questionsLists/questions.ts
@@ -0,0 +1,86 @@
+export const questions = [
+    {
+        "planet": [
+            {
+                "question": "In which year INSERT_NAME was born?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "birth_year",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "What height is INSERT_NAME?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "height",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "What planet is INSERT_NAME from?",
+                "requiredAnotherApiShot": "true",
+                "requiredAnswer": "homeworld",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "How much does INSERT_NAME weigh?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "mass",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "What color are INSERT_NAME's hair?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "hair_color",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "What color are INSERT_NAME's eyes?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "eye_color",
+                "possibleNAOrUnknownAnswers": ""
+            }
+        ],
+        "planets": [
+            {
+                "question": "What is the rotation period of INSERT_NAME?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "rotation_period",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "What is the orbital period of INSERT_NAME?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "orbital_period",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "What diameter is INSERT_NAME?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "diameter",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "What is the climate like at INSERT_NAME?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "climate",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "How much gravity is on INSERT_NAME?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "gravity",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "What kind of terrain prevails on INSERT_NAME?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "terrain",
+                "possibleNAOrUnknownAnswers": ""
+            },
+            {
+                "question": "What is the population on INSERT_NAME?",
+                "requiredAnotherApiShot": "false",
+                "requiredAnswer": "population",
+                "possibleNAOrUnknownAnswers": ""
+            }
+        ]
+    }
+]
